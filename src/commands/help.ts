1404520cb9ec470e5783b2d74976f0fa5e62642b
Crysp <just.lyonya@gmail.com>
2022-12-14 15:27:52 +0400

    Precompile command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,3 +1,6 @@
-export default async function help() {
+import TelegramBot from 'node-telegram-bot-api';
+
+export default async function help(bot: TelegramBot, chatId: number) {
+  console.log('Requested help for', chatId);
   return;
 }
